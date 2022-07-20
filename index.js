8baa64f3093ae6928df8f62fb262134c0b63844f
Roy4th <97925568+Roy4th@users.noreply.github.com>
2022-07-19 20:02:35 -0500

    index.js complete with questions for user

--- index.js
+++ index.js
@@ -1,13 +1,138 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const gernerateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+const questions = () => {
+  inquirer
+    .prompt([
+      //title
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project.",
+        validate: (projectTitle) => {
+          if (projectTitle) {
+            return true;
+          } else {
+            console.log("You must enter the name of your project!");
+            return false;
+          }
+        },
+      },
+      //description
+      {
+        type: "input",
+        name: "description",
+        message: "Enter a description for your project.",
+        validate: (projectDescription) => {
+          if (projectDescription) {
+            return true;
+          } else {
+            console.log("You must enter a description for your project!");
+            return false;
+          }
+        },
+      },
+      //installation
+      {
+        type: "input",
+        name: "installation",
+        message: "Explain how other users can install your project.",
+        validate: (projectInstallation) => {
+          if (projectInstallation) {
+            return true;
+          } else {
+            console.log("You must explain how to install your project!");
+            return false;
+          }
+        },
+      },
+      //Usage
+      {
+        type: "input",
+        name: "usage",
+        message: "Explain how to use your project/ application.",
+        validate: (projectUsage) => {
+          if (projectUsage) {
+            return true;
+          } else {
+            console.log("Please explain how to use your project/ application!");
+            return false;
+          }
+        },
+      },
+      //license
+      {
+        type: "checkbox",
+        name: "license",
+        message: "What License did you use for the project?",
+        choices: ["MIT", "GPLv2", "Apache", "GPlv3", "BSD 3-clause", "None"],
+      },
+      //contribute
+      {
+        type: "input",
+        name: "contribute",
+        message:
+          "What does the user need to know to help contribute to the project.",
+        validate: (projectContributors) => {
+          if (projectContributors) {
+            return true;
+          } else {
+            console.log(
+              "How would you like other user Contribute to your Project?"
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Enter your Github username.",
+        validate: (projectUsername) => {
+          if (projectUsername) {
+            return true;
+          } else {
+            console.log("Please enter your github username!");
+            return false;
+          }
+        },
+      },
+      //github : questions
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your Github URL.",
+        validate: (projectUsage) => {
+          if (projectUsage) {
+            return true;
+          } else {
+            console.log("Please enter your Github URL!");
+            return false;
+          }
+        },
+      },
+      //email : questions
+      {
+        type: "input",
+        name: "email",
+        message: "Enter an email for others to contact you.",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      fs.writeFile("./dist/README.md", generateMarkdown(data), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("success");
+        }
+      });
+    });
+};
 
 // Function call to initialize app
-init();
+questions();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let renderLicenseBadge = (license) => {
+  console.log(license);
+  if (license === "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "GPLv2") {
+    return "![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)";
+  } else if (license === "Apache") {
+    return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+  } else if (license === "GPlv3") {
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)";
+  } else if (license === "BSD 3-clause") {
+    return "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+let renderLicenseLink = (license) => {
+  if (license === "MIT") {
+    return `MIT
+Copyright (c) ${new Date().getFullYear()}
+      
+for more information about MIT License click [link](https://opensource.org/licenses/MIT)`;
+  } else if (license === "GPLv2") {
+    return `GPLv2
+Copyright (c) ${new Date().getFullYear()}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+for more information about GPlv2 license click [Link](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+  } else if (license === "Apache") {
+    return `Apache
+Copyright (c) ${new Date().getFullYear()}
+
+for more information about Apache license click [Link](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "GPlv3") {
+    return `GPLv3
+Copyright (c) ${new Date().getFullYear()}
+
+for more information about GPLv3 license click [Link](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "BSD 3-clause") {
+    return `BSD 3-clause
+Copyright (c) ${new Date().getFullYear()}
+
+for more information about BSD 3-clause license click [Link](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (license === "None") {
+    return `This application doesnt have any form of licensing`;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
-}
+  ${renderLicenseBadge(data.license[0])}
+  # Description
+  ${data.description}
+  # Table of Contents
+  * [Description](#discription)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Questions](#questions)
+  
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  # License
+  ${renderLicenseLink(data.license[0])}
+  # Contribution
+  ${data.contribute}
+  # Questions
+  If you have any question you can contact here: 
+  [${data.username}](${data.github})
+email: ${data.email}
+`
+};
 
 module.exports = generateMarkdown;
